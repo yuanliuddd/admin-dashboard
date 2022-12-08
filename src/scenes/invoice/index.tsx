8f539a9e2f958422f6186c49d42060e0e714f64e
YUAN LIU <yuanliu0120@gmail.com>
2022-12-08 16:36:58 +0100

    feat:invoices page init

--- src/scenes/invoice/index.tsx
+++ src/scenes/invoice/index.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { Box, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../config/theme";
+import Header from "../../components/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { mockDataInvoices } from "../../data/mockData";
 
-function index() {
+function Invoices() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+      flex: 0.5,
+    },
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "phone", headerName: "Phone Number ", editable: true, flex: 1 },
+    { field: "email", headerName: "Email", editable: true, flex: 1 },
+    { field: "cost", headerName: "Cost", editable: true, flex: .5 },
+    { field: "date", headerName: "Date", editable: true, flex: .5 },
+  ];
+
   return (
-    <div>index</div>
-  )
+    <Box m="20px">
+      <Header title="Invoices" subTitle="Invoice Balance List " />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": { border: 0 },
+          "& .MuiDataGrid-cell": { borderBottom: 0 },
+          "& .name-column-cell": { color: colors.greenAccent[300] },
+          "& .MuiDataGrid-row": { border: 0 },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: colors.blueAccent[700],
+            border: "none",
+          },
+          "& .MuiDataGrid-vitualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+            "& .PrivateSwitchBase-input": { backgroundColor: `${colors.greenAccent[400]} !important` },
+          },
+        }}
+      >
+        <DataGrid
+          columns={columns}
+          rows={mockDataInvoices}
+          checkboxSelection
+        ></DataGrid>
+      </Box>
+    </Box>
+  );
 }
 
-export default index
+export default Invoices;
