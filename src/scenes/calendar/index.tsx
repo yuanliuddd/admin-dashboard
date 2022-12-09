7dc97d255167d1ea253d2d9d44fa91e3b2f0a41d
YUAN LIU <yuanliu0120@gmail.com>
2022-12-09 14:51:21 +0100

    feat : Calendar page set up

--- src/scenes/calendar/index.tsx
+++ src/scenes/calendar/index.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useState } from "react";
+import Header from "../../components/Header";
+import { tokens } from "../../config/theme";
+///////////////// Full Calendar
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+////////////////// MUI
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
 
-export default function index() {
+const Caldendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const dateClickHandler = (selected) => {
+    const title = prompt("please enter a new title for your event ");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselected();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}- ${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const eventClickHandler = (selected) => {
+    if (window.confirm("Delete Event ?")) selected.event.removed();
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <Box m="20px">
+      <Header title="Calendar" subTitle="Calendar View Page " />
+      <Box width="300px">
+        <List>
+          <ListItem>
+            <ListItemText>s</ListItemText>
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+export default Caldendar;
